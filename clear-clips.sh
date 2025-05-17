1c679629cf07f96be0c1cc61103324def9209efa
Firepup Sixfifty <firepyp650@gmail.com>
2025-05-16 19:31:08 -0500

    xclip manipulation scripts (!! written while I had no internet !!)

---
+++ clear-clips.sh
@@ -0,0 +1,4 @@
+xclip -selection primary /dev/null
+xclip -selection secondary /dev/null
+xclip -selection clipboard /dev/null
+xclip -selection cut-buffer /dev/null
---
+++ show-clips.sh
@@ -0,0 +1,16 @@
+echo "== !! == primary             == !! =="
+CLIP=$(xclip -selection primary -o)
+echo -n $CLIP
+[[ "$CLIP" == "12" ]]&&echo "== !! == Trailing newline    == !! =="||[[ "$CLIP" == "" ]]&&echo "== !! == Clip is NULL        == !! =="||echo -e "\n== !! == No trailing newline == !! =="
+echo "== !! == secondary           == !! =="
+CLIP=$(xclip -selection secondary -o)
+echo -n $CLIP
+[[ "$CLIP" == "12" ]]&&echo "== !! == Trailing newline    == !! =="||[[ "$CLIP" == "" ]]&&echo "== !! == Clip is NULL        == !! =="||echo -e "\n== !! == No trailing newline == !! =="
+echo "== !! == clipboard           == !! =="
+CLIP=$(xclip -selection clipboard -o)
+echo -n $CLIP
+[[ "$CLIP" == "12" ]]&&echo "== !! == Trailing newline    == !! =="||[[ "$CLIP" == "" ]]&&echo "== !! == Clip is NULL        == !! =="||echo -e "\n== !! == No trailing newline == !! =="
+echo "== !! == cut-buffer          == !! =="
+CLIP=$(xclip -selection cut-buffer -o)
+echo -n $CLIP
+[[ "$CLIP" == "12" ]]&&echo "== !! == Trailing newline    == !! =="||[[ "$CLIP" == "" ]]&&echo "== !! == Clip is NULL        == !! =="||echo -e "\n== !! == No trailing newline == !! =="
