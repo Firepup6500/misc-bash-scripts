6097a05d6ea8812aca5b1d98fa6c71d60f231fd7
Firepup Sixfifty <firepup650@firepup650.com>
2026-03-07 07:52:58 -0600

    Add script to work with my dotfiles for uptime display

---
+++ write-uptime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -r up _ < /proc/uptime
+s=${up%.*}
+d=$((s/86400))
+h=$(((s%86400)/3600))
+m=$(((s%3600)/60))
+sec=$((s%60))
+printf "%03d:%02d:%02d:%02d\n" "$d" "$h" "$m" "$sec" > /tmp/uptime
