35539c75208e2ba215cb05f27bbb891b104775bf
Firepup Sixfifty <firepup650@firepup650.com>
2026-03-10 12:08:44 -0500

    WIP hook linking script, uptime loop script

---
+++ link-git-hooks.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+if [ -d .git-hooks ]; then
+  printf 'Hooks!\n'
+else
+  printf "No git hooks found in the current directory to link.\n"
+fi
---
+++ uptime-loop.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+while true;do
+  read -r up _ < /proc/uptime
+  s=${up%.*}
+  d=$((s/86400))
+  h=$(((s%86400)/3600))
+  m=$(((s%3600)/60))
+  sec=$((s%60))
+  printf "UP: %03d:%02d:%02d:%02d\n" "$d" "$h" "$m" "$sec" > /tmp/uptime
+  sleep 1
+done
