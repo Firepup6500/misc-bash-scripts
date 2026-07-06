a0e34f11bb0ba5e95d499b08a6daf4a045b26702
Firepup Sixfifty <firepup650@firepup650.com>
2026-07-06 03:17:35 -0500

    Add a script to launch lxterm and give it an icon
    Useful for on i3, where it never reads the WM_HINTS for the icon, and
    instead only reads the newer value (_NET_WM_ICON [iirc])

---
+++ ixterm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+lxterm "$@" &
+xtermpid=$!
+
+for i in {0..10}; do
+  # Code for getting an X window ID from a PID taken from: https://stackoverflow.com/a/44073873
+  while IFS= read line; do
+    if [[ "${line}" =~ (0x)([0-9a-z]+)([ ][- ][0-9]+[ ])([0-9]*) ]]; then
+      winId="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+      pid="${BASH_REMATCH[4]}"
+      if [[ "${pid}" -eq "${xtermpid}" ]]; then
+        WIND_IDS+=("${winId}")
+      fi
+    fi
+  done < <(wmctrl -lp)
+
+  if [ "${#WIND_IDS[@]}" -gt 0 ]; then
+    magick /usr/local/share/icons/xterm/terminal_16x16.xpm PNG:-|xseticon -id "${WIND_IDS[0]}" -
+    break
+  fi
+  sleep 0.1
+done
+wait
