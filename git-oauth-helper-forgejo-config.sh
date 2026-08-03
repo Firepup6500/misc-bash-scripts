69a45da22f9ca05a62489d321a6acb08847b0357
Firepup Sixfifty <firepup650@firepup650.com>
2026-08-02 19:36:09 -0500

    Fix mistake in last commit
    Accidentally quoted both arguments to a git config command instead of
    just the first one

--- git-oauth-helper-forgejo-config.sh
+++ git-oauth-helper-forgejo-config.sh
@@ -2,7 +2,7 @@
 
 printf "Enter the site name (no trailing slash, but include the leading https:// or http://):\n> "
 read -r sitename
-git config --global "credential.$sitename.oauthClientId a4792ccc-144e-407e-86c9-5e7d8d9c3269"
+git config --global "credential.$sitename.oauthClientId" a4792ccc-144e-407e-86c9-5e7d8d9c3269
 git config --global "credential.$sitename.oauthScopes" "read_repository write_repository"
 git config --global "credential.$sitename.oauthAuthURL" /login/oauth/authorize
 git config --global "credential.$sitename.oauthTokenURL" /login/oauth/access_token
